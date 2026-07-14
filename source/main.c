d5c6dc8c05b18987ae27332cecf1cc52afa2d5e6
Daviti Petriashvili <59766692+memory-hunter@users.noreply.github.com>
2026-07-14 13:13:53 +0200

    Add NDK_PORT ifdefs to switch easily to port NDK games

--- source/main.c
+++ source/main.c
@@ -6,8 +6,6 @@
 #include <falso_jni/FalsoJNI.h>
 #include <so_util/so_util.h>
 
-#include "reimpl/controls.h"
-
 int _newlib_heap_size_user = 256 * 1024 * 1024;
 
 #ifdef USE_SCELIBC_IO
@@ -16,17 +14,21 @@ int sceLibcHeapSize = 4 * 1024 * 1024;
 
 so_module so_mod;
 
-int main() {
+#ifndef NDK_PORT
+#include "reimpl/controls.h"
+int main()
+{
     soloader_init_all();
 
-    int (* JNI_OnLoad)(void *jvm) = (void *)so_symbol(&so_mod, "JNI_OnLoad");
+    int (*JNI_OnLoad)(void *jvm) = (void *)so_symbol(&so_mod, "JNI_OnLoad");
     JNI_OnLoad(&jvm);
 
     gl_init();
 
     // ... do some initialization
 
-    while (1) {
+    while (1)
+    {
         // ... render call
         gl_swap();
     }
@@ -34,14 +36,60 @@ int main() {
     sceKernelExitDeleteThread(0);
 }
 
-void controls_handler_key(int32_t keycode, ControlsAction action) {
+void controls_handler_key(int32_t keycode, ControlsAction action)
+{
     // Call into the .so here
 }
 
-void controls_handler_touch(int32_t id, float x, float y, ControlsAction action) {
+void controls_handler_touch(int32_t id, float x, float y, ControlsAction action)
+{
     // Call into the .so here
 }
 
-void controls_handler_analog(ControlsStickId which, float x, float y, ControlsAction action) {
+void controls_handler_analog(ControlsStickId which, float x, float y, ControlsAction action)
+{
     // Call into the .so here
 }
+#else
+#include <falso_ndk/FalsoNDK.h>
+int main()
+{
+    soloader_init_all();
+
+    // Load and call JNI_OnLoad so the game initialises its Java-side state
+    int (*JNI_OnLoad)(void *jvm) = (void *)so_symbol(&so_mod, "JNI_OnLoad");
+    JNI_OnLoad(&jvm);
+
+    gl_init();
+
+    // Build a fake ANativeActivity that the game's onCreate will receive
+    ANativeActivity *activity = malloc(sizeof(ANativeActivity));
+    activity->callbacks = malloc(sizeof(ANativeActivityCallbacks));
+    activity->env = &jni; // from FalsoJNI
+    activity->vm = &jvm;  // from FalsoJNI
+    activity->clazz = (jclass)0x42424242;
+    activity->internalDataPath = DATA_PATH "assets/";
+    activity->externalDataPath = DATA_PATH "assets/";
+    activity->sdkVersion = 14;
+    activity->instance = NULL;
+
+    // Drive the activity lifecycle
+    int (*ANativeActivity_onCreate)(ANativeActivity *, void *, size_t) =
+        (void *)so_symbol(&so_mod, "ANativeActivity_onCreate");
+    ANativeActivity_onCreate(activity, NULL, 0);
+
+    activity->callbacks->onStart(activity);
+    activity->callbacks->onResume(activity);
+
+    // Wire up input and the native window
+    AInputQueue *aInputQueue = AInputQueue_create();
+    activity->callbacks->onInputQueueCreated(activity, aInputQueue);
+
+    ANativeWindow *aNativeWindow = ANativeWindow_create();
+    activity->callbacks->onNativeWindowCreated(activity, aNativeWindow);
+
+    activity->callbacks->onWindowFocusChanged(activity, 1);
+
+    sceKernelExitDeleteThread(0);
+}
+#endif
